8e08c872af86c2fad8e458c22be90486d03ab62e
piyushaipl <piyushaipl@users.noreply.github.com>
2021-02-24 20:09:13 +0530

    new home page add

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,14 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  slideOpts ={}
+  slideOpts ={
+    slidesPerView: 4,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      shadowOffset: 20,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }
+    // cubeEffect: {
+    //   shadow: true,
+    //   slideShadows: true,
+    //   shadowOffset: 20,
+    //   shadowScale: 0.94,
+    // }
+  }
   slideItem=[];
   constructor() {
 
-    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Amizara Vasupujya (Ghatkopar)'})
-    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Munisvrat Derasar (Matunga)'})
-    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Shantinath Derasar (Sion)'})
-    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Parshvanath Derasar (Borival)'})
+    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Amizara Vasupujya ',loc:'Ghatkopar'})
+    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Munisvrat Derasar ',loc:'Matunga'})
+    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Shantinath Derasar ',loc:'Sion'})
+    this.slideItem.push({img:'../../../assets/img/slide_img.png',title:'Parshvanath Derasar ',loc:'Borival'})
 
    }
 
