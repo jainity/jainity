45df9ac31066af9d2cc46415ce8426dd4960e680
piyushaipl <piyushaipl@users.noreply.github.com>
2021-03-04 12:53:31 +0530

    new design changes for home page

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -3,12 +3,11 @@
     "use strict";
     $.fn.angularFunction = function() {
       
-    $(document).on('click', '.search_btn', function(e) {
-            e.preventDefault();
-            $("#header .search_block").toggleClass('open'); 
-            console.log('click  icons_click');   
-        });
-    };
+   
+
+  
+};
+   
 })(jQuery);
 
 $(window).angularFunction();
